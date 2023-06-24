dcb9278556cf1ac5776847a1a2517408952f86d2
Ayubpronub <gameoveranubys@gmail.com>
2023-06-24 17:14:09 +0500

    j

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print('Hello world')
+access = False
+while not access:
+    username = input("Enter username")
+    password = input("Enter password")
+    if username == 'Ayub' and password == '7772727':
+        print('Access granted')
+        access = True
+    elif username == 'Ayub'
+        print('Access denied, Wrong password ')
+    else:
+        print('Access denied, Wrong username'')
+print('End of program')
 
 
 
